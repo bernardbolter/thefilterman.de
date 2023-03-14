ecfcf4a32dde0a8efce2219a35c7436643491985
bernardbolter <bernardbolter@gmail.com>
2023-03-14 14:51:23 +0100

    added contact and footer info

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -24,92 +24,95 @@ const Contact = () => {
   }
   return (
       <section className={styles.container}>
-            <h1>Send me a message and</h1>
-            <h1>Lets see how we can make</h1>
-            <h1>your thing Smooth.</h1>
-            <p>by email</p>
-            <h2>smooth@smoothism.com</h2>
-            <p>or use the form</p>
-            <Formik
-                initialValues={{
-                  name: '', 
-                  email: '', 
-                  message: ''
-                }}
-                validationSchema={FormSchema}
-                onSubmit={
-                  async (
-                    values,
-                    { setSubmitting, resetForm }
-                  ) => {
-                    setSubmitting(true)
-                    const response = await fetch('/api/mail', {
-                      method: 'POST',
-                      body: JSON.stringify(values)
-                    })
-                    if (response.status === 200) {
-                      setResponseMessage("Your message has been sent, we will get back to you as soon as possible")
-                      resetForm()
-                      setFormSubmitted(true)
-                      setSubmitting(false)
-                    } else {
-                      setResponseMessage("There was an error sending you\'re message, please try again later or send an email directly to smooth@smoothism.com")
-                      resetForm()
-                      setFormSubmitted(true)
-                      setSubmitting(false)
+          <div className={styles.info}>
+              <h1>Send us a message and</h1>
+              <h1>Lets see how we can make</h1>
+              <p>by email</p>
+              <h2>gotfilter@thefilterman.de</h2>
+            </div>
+            <div className="form">
+              <p>or use the form:</p>
+              <Formik
+                  initialValues={{
+                    name: '', 
+                    email: '', 
+                    message: ''
+                  }}
+                  validationSchema={FormSchema}
+                  onSubmit={
+                    async (
+                      values,
+                      { setSubmitting, resetForm }
+                    ) => {
+                      setSubmitting(true)
+                      const response = await fetch('/api/mail', {
+                        method: 'POST',
+                        body: JSON.stringify(values)
+                      })
+                      if (response.status === 200) {
+                        setResponseMessage("Your message has been sent, we will get back to you as soon as possible")
+                        resetForm()
+                        setFormSubmitted(true)
+                        setSubmitting(false)
+                      } else {
+                        setResponseMessage("There was an error sending you\'re message, please try again later or send an email directly to smooth@smoothism.com")
+                        resetForm()
+                        setFormSubmitted(true)
+                        setSubmitting(false)
+                      }
                     }
                   }
-                }
-            >
-                {({ errors, touched, isSubmitting }) => {
-                    console.log("subing: ", isSubmitting)
-                    return (
-                    <Form name="smoothism-contact">
-                        <div className="form-input form-name">
-                            <label htmlFor="name">Name: </label>
-                            <Field
-                                id="name"
-                                name="name" 
-                                onFocus={handleFocus}
-                            />
-                            {(errors.name && touched.name) && <div className="error-message">{errors.name}</div>}
-                        </div>
-                        <div className="form-input form-email">
-                            <label htmlFor="email">Email: </label>
-                            <Field 
-                                id="email"
-                                name="email" 
-                                onFocus={handleFocus}    
-                            />
-                            {(errors.email && touched.email) && <div className="error-message">{errors.email}</div>}
-                        </div>
-                        <div className="form-input form-message">
-                            <label htmlFor="message">Message: </label>
-                            <Field
-                                id="message"
-                                name="message" 
-                                component="textarea"
-                                onFocus={handleFocus}    
-                            />
-                            {(errors.message && touched.message) && <div className="error-message">{errors.message}</div>}
-                        </div>
-                        {isSubmitting ? (
-                            <div className="form-blocks">
-                                <ThreeDots />
-                            </div>
-                        ) : (
-                            <>
-                                <button 
-                                    className="send-button" 
-                                    type="submit"  
-                                >Send</button>
-                                <p>{responseMessage.length !== 0 && responseMessage}</p>
-                            </>
-                        )}
-                        
-                    </Form>
-                )}}
-            </Formik>
+              >
+                  {({ errors, touched, isSubmitting }) => {
+                      console.log("subing: ", isSubmitting)
+                      return (
+                      <Form name="smoothism-contact">
+                          <div className="form-input form-name">
+                              <label htmlFor="name">Name: </label>
+                              <Field
+                                  id="name"
+                                  name="name" 
+                                  onFocus={handleFocus}
+                              />
+                              {(errors.name && touched.name) && <div className="error-message">{errors.name}</div>}
+                          </div>
+                          <div className="form-input form-email">
+                              <label htmlFor="email">Email: </label>
+                              <Field 
+                                  id="email"
+                                  name="email" 
+                                  onFocus={handleFocus}    
+                              />
+                              {(errors.email && touched.email) && <div className="error-message">{errors.email}</div>}
+                          </div>
+                          <div className="form-input form-message">
+                              <label htmlFor="message">Message: </label>
+                              <Field
+                                  id="message"
+                                  name="message" 
+                                  component="textarea"
+                                  onFocus={handleFocus}    
+                              />
+                              {(errors.message && touched.message) && <div className="error-message">{errors.message}</div>}
+                          </div>
+                          {isSubmitting ? (
+                              <div className="form-blocks">
+                                  <ThreeDots />
+                              </div>
+                          ) : (
+                              <>
+                                  <button 
+                                      className="send-button" 
+                                      type="submit"  
+                                  >Send</button>
+                                  <p>{responseMessage.length !== 0 && responseMessage}</p>
+                              </>
+                          )}
+                          
+                      </Form>
+                  )}}
+              </Formik>
+            </div>
       </section>
   )
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,23 @@
+import SuperMask from '@/svg/super-mask'
+
 import styles from './footer.module.scss'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <h1>Footer</h1>
+      <div className={styles.title}>
+        <SuperMask />
+        <h1>The Filterman</h1>        
+      </div>
+      <div className={styles.address}>
+
+      </div>
+      <div className={styles.contact}>
+
+      </div>
+      <div className={styles.links}>
+
+      </div>
     </div>
   )
 }
